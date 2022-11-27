eb56eb80e849dc0320446a0a29b11acb007dc829
Andrii Banduliak <banduliak1303@gmail.com>
2022-11-27 12:01:40 +0300

    task # 2

--- Program.cs
+++ Program.cs
@@ -2,21 +2,22 @@
 Clear();
 
 int[] arr = new int[4];
-
 fillArr(arr);
-WriteLine($"[{String.Join(",", arr)}]-> {evenCount(arr)}");
+WriteLine($"[{String.Join(",", arr)}]-> {oddSum(arr)}");
 
 void fillArr(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(100, 1000);
+        arr[i] = new Random().Next(0, 51);
     }
 }
-int evenCount(int[] arr)
+int oddSum(int[] arr)
 {
-    int counter = 0;
-    foreach (int i in arr)
-        if (i % 2 == 0) counter++;
-    return counter;
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 0) sum += arr[i];
+    }
+    return sum;
 }
